a61c9524a9bd95ee3c9e941fe6fe5b4f64b9e9fd
MiraCho <miracho119@gmail.com>
2024-08-30 23:57:15 -0600

    add calculator input in html

---
+++ mortgate-calculator.js
@@ -0,0 +1,5 @@
+const loanAmount = document.querySelector('#loanAmount');
+const interestRate = document.querySelector('#interestRate');
+const loanTerm = document.querySelector('#loanTerm');
+const calculateBtn = document.querySelector('#calculateBtn');
+
